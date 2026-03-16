a2226ecfbff11b9cfd2d2f81e55773268d7a6b22
miran11x <miran.mohammed111@gmail.com>
2026-03-16 22:21:33 +0300

    Update API URL for production

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'http://localhost:3001/api' });
+const api = axios.create({ baseURL: 'https://runaki-kb-backend-production.up.railway.app/api' });
 
 api.interceptors.request.use(cfg => {
   const token = localStorage.getItem('rk_token');
