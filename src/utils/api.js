e9dfa5c968b28ce3b84a0ad44d3c0acbf6340a12
miran11x <miran.mohammed111@gmail.com>
2026-03-21 17:42:20 +0300

    Force redeploy with correct API URL

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'https://runaki-kb-fsftl9g3m-miran11xs-projects.vercel.app/api' });
+const api = axios.create({ baseURL: 'https://runaki-kb-api.vercel.app/api' });
 
 api.interceptors.request.use(cfg => {
   const token = localStorage.getItem('rk_token');
